0b3bb0908c43b59a84ca2b40cc8e70f9b27363c3
Dima <lozhnii@yandex.ru>
2025-06-02 00:08:35 +0300

    Minor update

--- src/coords.cpp
+++ src/coords.cpp
@@ -10,13 +10,13 @@ CartesianCoordinates geod_to_cart(const GeodeticCoordinates geod_coords) {
     double sin_lat = sin(geod_coords.latitude);
     double cos_lon = cos(geod_coords.longitude);
     double sin_lon = sin(geod_coords.longitude);
-    double n = R_eq/sqrt(1-E_2*sin_lat*sin_lat);
+    double n_lat = R_eq/sqrt(1-E_2*sin_lat*sin_lat);
 
     CartesianCoordinates cart_coords;
 
-    cart_coords.x = (n + geod_coords.altitude)*cos_lat*cos_lon;
-    cart_coords.y = (n + geod_coords.altitude)*cos_lat*sin_lon;
-    cart_coords.z = (((R_pol*R_pol)/(R_eq*R_eq))*n + geod_coords.altitude)*sin_lat;
+    cart_coords.x = (n_lat + geod_coords.altitude)*cos_lat*cos_lon;
+    cart_coords.y = (n_lat + geod_coords.altitude)*cos_lat*sin_lon;
+    cart_coords.z = (((R_pol*R_pol)/(R_eq*R_eq))*n_lat + geod_coords.altitude)*sin_lat;
 
     return cart_coords;
 };
